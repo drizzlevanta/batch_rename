0539b691246197fe0def5ff2a24e2443f068cab5
Luxi <luxi.zheng@siemens-healthineers.com>
2023-03-17 17:27:35 -0400

    Accept command line args

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,15 @@ use clap::Parser;
 
 pub fn batch_rename(params: Params) -> io::Result<usize> {
     println!("params:{:?}", params);
-    let mut files = access_dir(params.dir_path)?;
+    let mut files = access_dir(&params.dir_path)?;
     sort_dir(&mut files, params.alphanumeric_sort);
-    rename_dir(&files, &params.batch_name, params.extension.as_deref())
+    rename_dir(
+        &files,
+        &params.batch_name,
+        params.extension.as_deref(),
+        &params.dir_path,
+    )
 }
-// pub fn batch_rename<P>(path: P, new_batch_name: &str) -> io::Result<usize>
-// where
-//     P: AsRef<Path>,
-// {
-//     let mut files = access_dir(path)?;
-//     sort_dir(&mut files);
-//     rename_dir(&files, new_batch_name)
-// }
 
 fn access_dir<P>(path: P) -> io::Result<Vec<PathBuf>>
 where
@@ -40,14 +37,24 @@ fn sort_dir(files: &mut Vec<PathBuf>, is_alphanumeric_sort: bool) {
     }
 }
 
-fn rename_dir(files: &Vec<PathBuf>, new_batch_name: &str, ext: Option<&str>) -> io::Result<usize> {
+fn rename_dir<P>(
+    files: &Vec<PathBuf>,
+    new_batch_name: &str,
+    ext: Option<&str>,
+    path: P,
+) -> io::Result<usize>
+where
+    P: AsRef<Path>,
+{
+    fs::rename("a.txt", "a.txt")?;
+    // let path_name = file.parent().unwrap().display();
+    let path = path.as_ref().display();
     for (index, file) in files.iter().enumerate() {
-        let new_file_name = file.parent().unwrap().display();
         fs::rename(
             file,
             format!(
                 r"{}\{}_{}.{}",
-                new_file_name,
+                path,
                 new_batch_name,
                 index + 1,
                 ext.unwrap_or_else(|| { file.extension().unwrap_or_default().to_str().unwrap() })
@@ -62,18 +69,18 @@ fn rename_dir(files: &Vec<PathBuf>, new_batch_name: &str, ext: Option<&str>) -> 
 
 #[derive(Debug, Default, Parser)]
 #[clap(author = "LZ", version = "1.0.0", about)]
-///Batch rename files in a directory. Files names use auto-number as suffix
+///Batch rename files in a directory, with auto-numbering as suffix.
 pub struct Params {
     #[clap(short = 'd', long = "dir-path")]
     #[clap(default_value = ".")]
     ///Path to the directory. Defaults to current directory
     pub dir_path: String,
 
-    ///Prefix to be used for the renaming
+    ///Prefix for the renaming
     // #[clap(short = 'n', long = "batch-name")]
     pub batch_name: String,
 
-    ///Using alphanumeric sort to rename files. Defaults to true. If set to false, regular sort will be used.
+    ///Using alphanumeric sort prior to renaming files. Defaults to true. If set to false, regular sort will be used.
     #[clap(short = 'a', long = "alphanumeric-sort")]
     #[clap(default_value_t = true)]
     pub alphanumeric_sort: bool,
@@ -83,30 +90,30 @@ pub struct Params {
     pub extension: Option<String>,
 }
 
-impl Params {
-    pub fn build<A>(args: &mut A) -> Result<Params, &'static str>
-    where
-        A: Iterator<Item = String>,
-    {
-        args.next();
-        let dir_path: String;
-        let batch_name: String;
+// impl Params {
+//     pub fn build<A>(args: &mut A) -> Result<Params, &'static str>
+//     where
+//         A: Iterator<Item = String>,
+//     {
+//         args.next();
+//         let dir_path: String;
+//         let batch_name: String;
 
-        match args.next() {
-            Some(arg) => dir_path = arg,
-            None => return Err("No directory path provided"),
-        }
+//         match args.next() {
+//             Some(arg) => dir_path = arg,
+//             None => return Err("No directory path provided"),
+//         }
 
-        match args.next() {
-            Some(arg) => batch_name = arg,
-            None => return Err("No batch name provided"),
-        }
+//         match args.next() {
+//             Some(arg) => batch_name = arg,
+//             None => return Err("No batch name provided"),
+//         }
 
-        Ok(Params {
-            dir_path,
-            batch_name,
-            alphanumeric_sort: true,
-            extension: Some(String::from("jpg")),
-        })
-    }
-}
+//         Ok(Params {
+//             dir_path,
+//             batch_name,
+//             alphanumeric_sort: true,
+//             extension: Some(String::from("jpg")),
+//         })
+//     }
+// }
